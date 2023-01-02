b327b0adb048be5ad2fb41a930326448a0071f45
Dario Bear Bardales <73848607+Bearbar00008@users.noreply.github.com>
2023-01-02 10:27:00 -0600

    Update useRedirectLogin.ts
    LogIng problem when you did log out fixed

--- frontend/apps/login/hooks/useRedirectLogin.ts
+++ frontend/apps/login/hooks/useRedirectLogin.ts
@@ -2,10 +2,13 @@ import { useLocalStorage, useRedirect } from "../../utils/hooks"
 import { useEffect } from "react"
 import { useLoginSelector } from "../features/hooks"
 import { selectLoginPageReducer } from "../features/appLogin"
-
+import { useLoginDispatch } from "../features/hooks"
+import { setIsAuthenticated } from "../features/authentication"
 
 export default (route: string) => {
     
+    const LoginDispatch = useLoginDispatch()
+
     const { deleteLocalStorage } = useLocalStorage()
 
     const loginSelector = useLoginSelector(selectLoginPageReducer)
@@ -17,6 +20,10 @@ export default (route: string) => {
         if(authenticated == "authenticated"){
             deleteLocalStorage("redirect")
             redirectPath(route)
+        }
+
+        return () => {
+            LoginDispatch(setIsAuthenticated('idle'))
         }
 
     },[authenticated])
