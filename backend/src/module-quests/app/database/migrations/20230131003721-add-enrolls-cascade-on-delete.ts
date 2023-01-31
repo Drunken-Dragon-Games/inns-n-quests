ee3e42353dcc4dcdf31b8e470104256ac8d82859
Vledic | Franco <atfm05@gmail.com>
2023-01-30 18:45:35 -0600

    fixed bug related to adventurers that were changed from wallet

---
+++ backend/src/module-quests/app/database/migrations/20230131003721-add-enrolls-cascade-on-delete.ts
@@ -0,0 +1,50 @@
+import { DataTypes } from 'sequelize'
+import { Migration } from '../migration_scripts/migrate'
+
+export const up: Migration = async ({ context: sequelize }) => {
+  await sequelize.getQueryInterface().changeColumn('enrolls', 'taken_quest_id', {
+      type: DataTypes.UUID,
+      onDelete: 'CASCADE',
+      references: {
+          model: {
+            tableName: "taken_quests"
+          },
+          key: 'id'
+      }
+  })
+  await sequelize.getQueryInterface().changeColumn('enrolls', 'adventurer_id', {
+      type: DataTypes.UUID,
+      onDelete: 'CASCADE',
+      unique: true,
+      primaryKey: true,
+      references: {
+          model: {
+            tableName: "adventurers"
+          },
+          key: "id"
+      }
+  })
+}
+
+export const down: Migration = async ({ context: sequelize }) => {
+  await sequelize.getQueryInterface().changeColumn('enrolls', 'taken_quest_id', {
+      type: DataTypes.UUID,
+      references: {
+          model: {
+            tableName: "taken_quests"
+          },
+          key: 'id'
+      }
+  })
+  await sequelize.getQueryInterface().changeColumn('enrolls', 'adventurer_id', {
+      type: DataTypes.UUID,
+      unique: true,
+      primaryKey: true,
+      references: {
+          model: {
+            tableName: "adventurers"
+          },
+          key: "id"
+      }
+  })
+}
