ab91c4c637ffeed37f8a9ad899ddad4f5e8dcefc
Vledic | Franco <atfm05@gmail.com>
2023-04-18 16:09:31 -0600

    minimal

--- backend/src/module-ddu-app/settings.ts
+++ backend/src/module-ddu-app/settings.ts
@@ -10,7 +10,7 @@ type StaticOrigin = boolean | string | RegExp | (boolean | string | RegExp)[]
 export const corsOptions = {
     origin: (requestOrigin: string | undefined, callback: (err: Error | null, origin?: StaticOrigin) => void) => {
         if (corsOrigins.includes(requestOrigin ?? "")) callback(null, true)
-        else callback(new Error('Not allowed by CORS'))
+        else callback(new Error(`Not allowed by CORS for ${requestOrigin} (allowed: ${corsOrigins})`))
     },
     allowedHeaders: ["Content-Type", "Authorization", "Origin", "Accept", "X-Requested-With", "Trace-ID"],
     credentials: true 
