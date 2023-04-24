992de75ee4ad9ea05fe7ec323b1187dd578289fb
Santiago Torres <santiago_oziel92@hotmail.com>
2023-04-24 13:13:14 -0600

    added governacne service factory methotds

--- backend/src/service-governance/service.ts
+++ backend/src/service-governance/service.ts
@@ -3,8 +3,12 @@ import { QueryInterface, Sequelize } from "sequelize"
 import { Umzug } from "umzug"
 import { buildMigrator } from "../tools-database"
 import { GovernanceService } from "./service-spec"
+import dotenv from "dotenv"
 
 import * as models from "./models"
+import * as ballotDB from "./ballots/ballots-db"
+
+import { config, HealthStatus } from "../tools-utils"
 
 export type GovernanceServiceDependencies = {
     database: Sequelize
@@ -24,10 +28,26 @@ export class GovernanceServiceDsl implements GovernanceService {
         const migrationsPath: string = path.join(__dirname, "migrations").replace(/\\/g, "/")
         this.migrator = buildMigrator(database, migrationsPath)
     }
+    static async loadFromEnv(dependencies: GovernanceServiceDependencies): Promise<GovernanceService> {
+        dotenv.config()
+        return await GovernanceServiceDsl.loadFromConfig({}, dependencies)
+    }
+    static async loadFromConfig(servConfig: GovernanceServiceConfig, dependencies: GovernanceServiceDependencies): Promise<GovernanceService> {
+        const service = new GovernanceServiceDsl( dependencies.database)
+            
+        await service.loadDatabaseModels()
+        return service
+    }
 
-    async loadDatabaseModels(): Promise<void> {}
+    async loadDatabaseModels(): Promise<void> {
+        ballotDB.configureSequelizeModel(this.database)
+        await this.migrator.up()
+    }
 
-    async unloadDatabaseModels(): Promise<void> {}
+    async unloadDatabaseModels(): Promise<void> {
+        await this.migrator.down()
+        await this.database.close()
+    }
 
     async addBallot(ballot: models.Ballot):Promise<models.AddBallotResponse>{
         return {state: "error", reason: "not implemented yet"}
