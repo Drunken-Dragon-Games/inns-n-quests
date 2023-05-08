640a21c23d4bb70576d80015154b3b82130d3021
Vledic | Franco <atfm05@gmail.com>
2023-05-08 09:12:50 -0600

    tos

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -8,6 +8,7 @@ const Home: NextPage = () =>
             <title>Drunken Dragon: Inns & Quests</title>
             <meta name="Drunken Dragon Inns & Quests" content="A Drunken Dragon Universe Game" />
             <script
+                defer
                 type="text/javascript"
                 src="https://app.termly.io/embed.min.js"
                 data-auto-block="on"
