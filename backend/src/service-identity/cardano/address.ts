c92d3b42b96fd59626e495c59556d11579919fe5
Vledic | Franco <atfm05@gmail.com>
2023-05-26 03:37:15 -0600

    blockfrost env for frontend

--- backend/src/service-identity/cardano/address.ts
+++ backend/src/service-identity/cardano/address.ts
@@ -6,5 +6,5 @@ export const validateStakeAddress = (stakeAddress: string, network: string): boo
         stakeAddress.length == 59 && !stakeAddress.includes("test") && !specialChars.test(stakeAddress)
     const testnetValidation = (): boolean =>
         stakeAddress.length == 64 && stakeAddress.includes("test") && !specialChars.test(stakeAddress)
-    return (network === "mainnet" ? mainnetValidation() : testnetValidation())
+    return (network === "Mainnet" ? mainnetValidation() : testnetValidation())
 }
