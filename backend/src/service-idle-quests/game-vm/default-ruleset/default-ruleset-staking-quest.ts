604ded7612b6dcaca437ad23d17a8991f134696e
Vledic | Franco <atfm05@gmail.com>
2023-05-27 05:36:22 -0600

    set idle quests to 5 hours

--- backend/src/service-idle-quests/game-vm/default-ruleset/default-ruleset-staking-quest.ts
+++ backend/src/service-idle-quests/game-vm/default-ruleset/default-ruleset-staking-quest.ts
@@ -79,7 +79,7 @@ export default class DefaultQuestRuleset implements StakingQuestRuleset {
 
     requirementInfo(requirement: StakingQuestRequirement): StakingQuestRequirementInfo {
         //const duration = Math.round(Math.max(30, apsSum(requirement.aps)))
-        const duration = 60 * 60 * 24 * 3
+        const duration = 60 * 60 * 5
         const currency = (
             Math.round(Math.max(1, apsSum(requirement.aps) / 10)) * 2 +
             (requirement.collection?.length ? (CharacterCollections.length - requirement.collection.length) : 0)  +
