b13a40fb169ad3109390aff39e25afd5c2eb398a
Santiago Torres <santiago_oziel92@hotmail.com>
2023-07-10 16:58:48 -0600

    defined contributions DB structure

---
+++ backend/src/service-collection/staking-contributions/contributions-db.ts
@@ -0,0 +1,80 @@
+import { DataTypes, Model, Sequelize } from "sequelize"
+
+export const dailyContributionsTableName = "collection_daily_contributions"
+export const weeklyContributionsTableName = "collection_weekly_contributions"
+
+export class DailyContribution extends Model {
+    declare dailyContributionId: string
+    declare contribution: string
+    declare userId: string
+    declare weeklyContributionId? : string
+}
+
+export const dailyContributionTableAttributes = {
+    dailyContributionId: {
+        type: DataTypes.STRING,
+        primaryKey: true,
+        unique: true
+    },
+    contribution: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    userId: {
+        type: DataTypes.UUID,
+        allowNull: false
+    },
+    weeklyContributionId: {
+        type: DataTypes.UUID,
+        allowNull: true,
+        references: {
+            model: weeklyContributionsTableName,
+            key: 'weeklyContributionId',
+        },
+    },
+}
+
+export class WeeklyContribution extends Model {
+    declare weeklyContributionId: string
+    declare contribution: string
+    declare userId: string
+}
+
+export const weeklyContributionTableAttributes = {
+    weeklyContributionId: {
+        type: DataTypes.STRING,
+        primaryKey: true,
+        unique: true
+    },
+    contribution: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    userId: {
+        type: DataTypes.UUID,
+        allowNull: false
+    },
+}
+
+export const configureSequelizeModel = (sequelize: Sequelize): void => {
+    DailyContribution.init(dailyContributionTableAttributes, {
+        sequelize,
+        modelName: "DailyContribution",
+        tableName: dailyContributionsTableName
+    })
+
+    WeeklyContribution.init(weeklyContributionTableAttributes, {
+        sequelize, 
+        modelName: 'WeeklyContribution', 
+        tableName: weeklyContributionsTableName
+    })
+
+    WeeklyContribution.hasMany(DailyContribution, {
+        foreignKey: "weeklyContributionId",
+        onDelete: "CASCADE"
+    })
+
+    DailyContribution.belongsTo(WeeklyContribution, {
+        foreignKey: "weeklyContributionId"
+    })
+}
