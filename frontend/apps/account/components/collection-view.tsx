7c63663774c824a5359f1c3313f9fe3a06ec91b4
Santiago Torres <santiago_oziel92@hotmail.com>
2023-07-17 15:45:53 -0600

    stash

--- frontend/apps/account/components/collection-view.tsx
+++ frontend/apps/account/components/collection-view.tsx
@@ -1,4 +1,19 @@
 import { UserInfo } from "../account-dsl"
+import styled from "styled-components"
+const CardContainer = styled.div`
+    width: 100%;
+    padding: 5vw;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 20px;
+
+    @media only screen and (max-width: 1400px) {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+`
 
 export const CollectionView = ({ userInfo }: { userInfo: UserInfo }) => 
     <>
