cb540affac506f676d2875159312088e7872e8b0
Santiago Torres <santiago_oziel92@hotmail.com>
2023-08-10 12:05:51 -0600

    Update mortal-view.tsx

--- frontend/apps/collection/mortal-display/mortal-view.tsx
+++ frontend/apps/collection/mortal-display/mortal-view.tsx
@@ -6,15 +6,16 @@ import { useState } from "react"
 const MortalCollectionContainer = styled.div`
     border: 2px solid #ccc;
     border-radius: 10px;
-    padding: 20px;
-    margin-bottom: 20px;
-    margin-left: 9.5vw;
+    padding: 10px; /* reduced from 20px */
+    margin-bottom: 10px; /* reduced from 20px */
+    margin-left: 25vw; /* reduced from 9.5vw */
     box-shadow: 0 10px 20px rgba(0, 0, 0, 0.1);
 
     display: grid;
     grid-template-columns: repeat(5, 1fr);
-    gap: 20px;
+    gap: 10px; /* reduced from 20px */
 `
+
 const Header = styled.div`
     display: flex;
     align-items: center;
@@ -32,7 +33,7 @@ const CollectibleContainer = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
-    gap: 10px;
+    gap: 5px;
 `;
 
 const CollectibleInfo = styled.div`
@@ -54,36 +55,60 @@ const capitalizeFirstLetter = (input: string): string => {
 
 export const MortalView = ({ collectionItems }: { collectionItems: CollectionWithGameData }) => {
     const dimensionsMap = {
-        "pixelTiles": {height: 10.5,  width: 6.75 },
-        "grandMasterAdventurers": { height: 12,width: 9.6 },
-        "adventurersOfThiolden": {height: 12,  width: 9 }
+        "pixelTiles": {height: 10.5 * 0.5,  width: 6.75 * 0.5 },
+        "grandMasterAdventurers": { height: 12 * 0.5, width: 9.6 * 0.5 },
+        "adventurersOfThiolden": {height: 12 * 0.5,  width: 9 * 0.5 }
     }
+    
     type PolicyName = "pixelTiles" | "adventurersOfThiolden" | "grandMasterAdventurers"
-    return(
-    <MortalCollectionContainer>
-        <Header>
-            <Title>Mortal Collection</Title>
-        </Header>
-        {Object.entries(collectionItems).map(([policyNameKey, itemsArray]) => {
-            const policyName = policyNameKey as PolicyName
-            return itemsArray.map((src, index) => (
-                <CollectibleContainer key={index}>
-                <MirroredPixelArtImage
-                    src={src.miniature}
-                    alt={src.name}
-                    height={dimensionsMap[policyName].height}
-                    width={dimensionsMap[policyName].width}
-                />
-                <CollectibleInfo>
-                    <p>{capitalizeFirstLetter(src.name)}</p>
-                    <p>Class: {src.class}</p>
-                    <p>APS: {src.aps.join(", ")}</p>
-                </CollectibleInfo>
-                </CollectibleContainer>
-            ));
-            })
-        }
 
-    </MortalCollectionContainer>
+    const renderAsset = (src: any, policyName: PolicyName) => {
+        const assetArray = [];
+        for (let i = 0; i < Number(src.quantity); i++) {
+            assetArray.push(
+                <CollectibleContainer key={`${src.name}-${i}`}>
+                    <MirroredPixelArtImage
+                        src={src.miniature}
+                        alt={src.name}
+                        height={dimensionsMap[policyName].height}
+                        width={dimensionsMap[policyName].width * (policyName === 'pixelTiles'&& src.class === 'furniture'? 1.5 : 1)}
+                    />
+                    <CollectibleInfo>
+                        <p>{capitalizeFirstLetter(src.name)}</p>
+                        <p>Class: {src.class}</p>
+                        <p>APS: {src.aps.join(", ")}</p>
+                    </CollectibleInfo>
+                </CollectibleContainer>
+            )
+        }
+        return assetArray;
+    }
+
+    return (
+        <MortalCollectionContainer>
+            <Header>
+                <Title>Mortal Collection</Title>
+            </Header>
+            <Header>
+                <Title>Adventurers</Title>
+            </Header>
+            {Object.entries(collectionItems).map(([policyNameKey, itemsArray]) => {
+                const policyName = policyNameKey as PolicyName
+                return itemsArray.map((src) => (
+                    src.class !== "furniture" && renderAsset(src, policyName)
+                ));
+            })}
+            
+            <Header>
+                <Title>Furniture</Title>
+            </Header>
+            {Object.entries(collectionItems).map(([policyNameKey, itemsArray]) => {
+                const policyName = policyNameKey as PolicyName
+                return itemsArray.map((src) => (
+                    src.class === "furniture" && renderAsset(src, policyName)
+                ));
+            })}
+        </MortalCollectionContainer>
     )
 }
+
