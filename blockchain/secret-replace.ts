42e804964d965fd8fe38f31cf77815cba24eaee9
Santiago Torres <santiago_oziel92@hotmail.com>
2023-08-22 13:10:45 -0600

    Update secret-replace.ts

--- blockchain/secret-replace.ts
+++ blockchain/secret-replace.ts
@@ -1,4 +1,4 @@
-
-/* const server = await Deno.readTextFile("./src/server.ts")
-const newServer = server.replace("9d8a1876f66da8b25753ef4b82cab693", Deno.env.get("ENCRYPTION_KEY") as string)
-await Deno.writeTextFile("./src/server.ts", newServer) */
+const server = await Deno.readTextFile("./src/server.ts")
+//const newServer = server.replace("9d8a1876f66da8b25753ef4b82cab693", Deno.env.get("ENCRYPTION_KEY") as string)
+const newServer = server.replace("9d8a1876f66da8b25753ef4b82cab693", "9d8a1876f66da8b25753ef4b82cab693")
+await Deno.writeTextFile("./src/server.ts", newServer)
